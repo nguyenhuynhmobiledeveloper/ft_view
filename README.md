516916e55e6d2aead7d8232d411ebcabad78f9af
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2025-01-07 14:05:03 +0700

    add DropDownButton2 and format code

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-//NGUYÊN TẮC LÀM 1 FILE DEMO ĐỂ HỌC
-Phải ghi nguồn + thời gian đầu tiên 
-Ghi thứ tự các bước làm vào readme
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
-import 'package:demo_ft_widget/effect_loading/app.dart';
 
 
-import 'package:demo_ft_widget/widget/widget_main.dart'; // File chứa tất cả các demo Widget
+
+
 
 import 'package:flutter/material.dart';
-import 'package:demo_ft_widget/view-library/tin_coder_view.dart';
+
+
+import 'drop_down/demo_DropdownButton2.dart';
 
 void main() async {
   // runApp(const MyApp());
 
-  runApp(const TinCoderMainView()); // tin_coder_view -- view-library
-  // runApp(const DemoWidget());    // folder widget
+  // runApp(const TinCoderMainView()); // tin_coder_view -- view-library
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blueGrey),
+      home: ( const DemoDropDownButton2()),
+    );
+  }
 }
