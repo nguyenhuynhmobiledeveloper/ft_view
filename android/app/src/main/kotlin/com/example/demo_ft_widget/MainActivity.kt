cf068a03dbc17ae938ae08e371d4c046875de462
nguyenhuynhdeveloper <nguyenhuynh.developer@gmail.com>
2025-02-18 22:34:53 +0700

    upgrade flutter 3.29.0 Dart 3.7.0

--- android/app/src/main/kotlin/com/example/demo_ft_widget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo_ft_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/iCloud/com/demo_ft_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package iCloud.com.demo_ft_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
